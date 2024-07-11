476398eb5fd708a4eb47eb11a65320a8749411ea
Akas Patranobis <akaspatranobis@yahoo.com>
2024-07-11 08:57:45 +0530

    upstream

---
+++ Conditions.sh
@@ -0,0 +1,82 @@
+We can test our logics using conditions or conditional statements
+
+
+I will take an umbrella if it rain today
+
+if [ condition ]; then
+ some commands
+else
+ some commands
+fi  
+
+if [ rain==true ]; then
+ I will take the umbrella
+else
+ I will not take
+fi  
+
+Operators---: [Numbers , Strings]  Number == 1,  || String = akas, akas patranobis
+
+Numbers:
+
+-eq ---- 4 is equal to 4 [ 4 -eq 4 ]
+-gt
+-lt
+-ne   --- not equals [ 4 -ne 5 ]
+
+Strings:
+
+==   Akas is equal to Vinay [ Akas == Vinay ]
+= a=Akas
+-z  Null or zero charater a=  \\  [ -z $a ]
+!=   [ Akas != Vinay ]
+
+
+x=$1  | is it > 100
+
+if [ $x -gt 100 ]; then
+ echo " $x is greater tahn 100"
+else
+ echo "$x is lesser thann 100"
+fi 
+
+More Dynamic ( beacuase I want to give my script some power so that it can take values from the user)
+
+1. Command line arguments  ----> bash con.sh 20 [ Postional argument or values 0,1,......]
+2. read --- read $a
+
+================
+
+x=$1
+
+echo "$0"
+echo "$1"
+
+if [ $x -gt 100 ]; then
+ echo " $x is greater tahn 100"
+else
+ echo "$x is lesser thann 100"
+fi
+
+======================
+
+read x
+
+echo ""The user passed this value: $x"
+
+if [ $x -gt 100 ]; then
+ echo " $x is greater tahn 100"
+else
+ echo "$x is lesser thann 100"
+fi
+
+=====================================
+
+read a
+read b
+
+if [ $a == $b ]; then
+ echo "Matched"
+else
+ echo "Not Matched"
+fi  
---
+++ variables.sh
@@ -0,0 +1,27 @@
+
+
+
+# A variable is an object that contains data used by one or more applications
+
+a=Vinay
+echo $a
+echo This is India , $a lives here
+
+
+b=HelLoWorld
+
+#Varibale with a special char data has to be put in "" quotes
+b="Hello World"
+
+# Command substitution ======================================
+
+pwd
+varname=$(command)
+
+a=$(pwd)
+
+# Arithmetic Expression ======= 
+1+2=3
++,-,*,/,%,^
+
+a=$((1+2))
